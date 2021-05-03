b76a3bcbb4e82914632467512e7d994a0484a377
patriziosbr <sbrozzi.patrizio@gmail.com>
2021-05-03 13:27:41 +0200

    fix in class

--- js/cognomi.js
+++ js/cognomi.js
@@ -7,18 +7,15 @@
 var cognomi = [ 'Neri', 'Bianchi', 'Rossi', 'Verdi', 'Gialli'];
 
 var askCogn = prompt('inserisci cognome');
-var askCogn = askCogn.charAt(0).toUpperCase() + askCogn ;
+
+askCogn = askCogn.charAt(0).toUpperCase() + askCogn.slice(1);
 
 // var cognomeUtente = prompt("Inserisci qui il tuo cognome")
 // var cognomeMaiusc= cognomeUtente.substring(0, 1).toUpperCase() + cognomeUtente.substring(1);
 
-
-
 cognomi.push(askCogn);
 
-
 cognomi.sort();
-
 
 for (var i = 0; i < cognomi.length; i++) {
     var umano = i + 1;
@@ -26,3 +23,13 @@ for (var i = 0; i < cognomi.length; i++) {
     console.log("il numero umano è: " + umano + " " + cognomi[i]);
 }
 
+// ciclo while 
+// var posizione = -1;
+
+// var x = 0;
+// while( x < cognomi.length ){
+//     if (cognomi[x] == askCogn) {
+//         posizione = i;
+//     }
+//     x++
+// }
